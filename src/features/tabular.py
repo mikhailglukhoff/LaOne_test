e433e55760035d69e5718f75e19fe82a9bd39960
mikhail <michaelglukhoff@gmail.com>
2025-09-04 19:53:20 +0200

    Update dataset usage instructions and enable verbose logging.

--- src/features/tabular.py
+++ src/features/tabular.py
@@ -65,7 +65,7 @@ def train_catboost(
         eval_metric="AUC",
         random_seed=42,
         class_weights=[1, 10],
-        verbose=False,
+        verbose=True,
         )
 
     model.fit(train_pool, eval_set=val_pool, early_stopping_rounds=50)
